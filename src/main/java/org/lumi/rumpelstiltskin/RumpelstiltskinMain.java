0d9b6aa8793ebb0dfd7d43cfc43d9f076e30d796
giannis20012001 <i.tsantilis@yahoo.com>
2016-02-19 12:54:59 +0200

    Bob is painting

---
+++ src/main/java/org/lumi/rumpelstiltskin/RumpelstiltskinMain.java
@@ -0,0 +1,11 @@
+package org.lumi.rumpelstiltskin;
+
+/**
+ * Created by lumi (A.K.A. John Tsantilis) on 19/2/2016.
+ */
+
+public class RumpelstiltskinMain {
+    public static void main(String[] args) {
+
+    }
+}
