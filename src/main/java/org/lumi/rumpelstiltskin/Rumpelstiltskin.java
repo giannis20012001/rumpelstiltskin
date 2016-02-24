dc9aaf4befe5a23b56a72a59293e9cf7a58a3595
John Tsantilis <i.tsantilis@yahoo.com>
2016-02-24 10:35:26 +0200

    Bob is painting

--- src/main/java/org/lumi/rumpelstiltskin/Rumpelstiltskin.java
+++ src/main/java/org/lumi/rumpelstiltskin/Rumpelstiltskin.java
@@ -1,14 +1,13 @@
 package org.lumi.rumpelstiltskin;
 
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created by John Tsantilis (A.K.A lumi) on 20/2/2016.
  */
 public class Rumpelstiltskin {
-    public void importBaseImage() {
-        //
-
-    }
-
     public void buildOSVImage() {
         //
 
@@ -20,5 +19,6 @@ public class Rumpelstiltskin {
     }
 
     public static final String DEFAULT_REPO_URL = "https://s3.amazonaws.com/osv.capstan/";
+    public static final Path DEFAULT_CONFIG_PATH = Paths.get("$HOME/.capstan");
 
 }
